bd1073e989dbb0341a38c60e1889ed9aeccdaefb
andeki <anders@klever-kirkeby.com>
2016-03-03 11:31:00 +0100

    Added back stupid stuff...

--- bt_server.py
+++ bt_server.py
@@ -12,7 +12,9 @@ port = server_sock.getsockname()[1]
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
 advertise_service( server_sock, "SampleServer",
-                   service_id=uuid
+                   service_id=uuid,
+                   service_classes=[uuid, SERIAL_PORT_CLASS],
+                   profiles=[SERIAL_PORT_PROFILE],
                     )
 
 print("Waiting for connection on RFCOMM channel %d" % port)
