7195bbc094253e26615edc0bb9937a8ff2d251c2
andeki <anders@klever-kirkeby.com>
2016-03-03 11:05:05 +0100

    Added the bluetooth server (WIP)

---
+++ bt_server.py
@@ -0,0 +1,41 @@
+from bluetooth import *
+
+# DISCLAIMER - if you do not have pybluez installed there will be a lot of red lines in the following code
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+# TODO Figure out whether to use MAC addresses or UUID for identifying the connections.
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service( server_sock, "SampleServer",
+                   service_id=uuid,
+                   service_classes=[uuid, SERIAL_PORT_CLASS],
+                   profiles=[SERIAL_PORT_PROFILE],
+                    )
+
+print("Waiting for connection on RFCOMM channel %d" % port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from ", client_info)
+
+
+# Right now it is receiving data - change the "sock.recv" if you want it to send
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if len(data) == 0: break
+        print("received [%s]" % data)
+except IOError:
+    pass
+
+print("disconnected")
+
+client_sock.close()
+server_sock.close()
+print("all done")
+
+# TODO Make a file reader to read in the raw OBD2-data and send them to the bluetooth socket
