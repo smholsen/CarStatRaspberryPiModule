0889835f0a07dba47c51a23dcfc31b9e4317df23
andeki <anders@klever-kirkeby.com>
2016-04-05 12:02:25 +0200

    added json reader/sender

--- bt_server.py
+++ bt_server.py
@@ -44,14 +44,16 @@ def send_json_data(socket):
     with open('Border_Roads.json', 'r+') as file:
         for line in file:
             data = json.loads(line)
+            datajson = json.dumps(data)
+
 
             # Timestamp of JSON data
             stamp = time.gmtime(data['timestamp'])
 
             if data['name'] == 'vehicle_speed':
                 # Send JSON data
-                print(data)
-                send_message(socket, data)
+                print(datajson)
+                send_message(socket, datajson)
                 print('Sent data @ {}: {}'.format(stamp, data))
                 time.sleep(0.01)
 
